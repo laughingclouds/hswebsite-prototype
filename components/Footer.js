07d28c477a5f0306bbc478d2e81198382eee806b
laughingclouds <zenithtaiga@gmail.com>
2022-09-10 10:47:38 +0530

    Create footer Used in shared component More work to be done

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer({ className = "" }) {
+  const classes = "" + className;
+  
+  return (
+    <footer className={classes}>
+      <p>lorem ipsum doler</p>
+    </footer>
+  );
+}
