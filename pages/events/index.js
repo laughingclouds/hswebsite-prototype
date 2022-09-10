3e83f227f5437094b4f428eb0ad8c1e629327519
laughingclouds <zenithtaiga@gmail.com>
2022-09-10 10:49:28 +0530

    Create pages - / - /join-hs - /events/

---
+++ pages/events/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Events() {
+  return (
+    <>
+      <Head>
+        <title>Events | Hackoverflow Society</title>
+      </Head>
+
+      <main>
+        <p>lol</p>
+      </main>
+    </>
+  );
+}
---
+++ pages/join-hs.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function JoinHS() {
+  return (
+    <>
+      <Head>
+        <title>Join Us | Hackoverflow Society</title>
+      </Head>
+
+      <h1>Ready to join HS?</h1>
+      <p>Great! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet mollis ante. Vivamus luctus id tellus at posuere. Cras pharetra accumsan elit, eu tincidunt enim rhoncus non. Aenean finibus ultricies tellus, viverra lacinia nibh. Pellentesque mollis turpis ornare eros vehicula cursus. Quisque auctor orci nec lacus pharetra molestie. Cras quis quam ut nulla imperdiet cursus. Sed dictum sollicitudin facilisis. Integer pellentesque condimentum aliquet. Donec eget sollicitudin ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vehicula vulputate libero nec posuere. Proin luctus varius nibh. Sed a eleifend risus. Cras vel magna tempor, maximus erat vel, tincidunt leo. Vestibulum eget augue a ligula maximus rhoncus sit amet luctus ipsum.</p>
+    </>
+  );
+}
